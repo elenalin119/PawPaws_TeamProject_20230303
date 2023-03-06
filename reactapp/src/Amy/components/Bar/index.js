60a2a453fe347072e2c383c3359674a7a14a2fbd
quanhuiyang <quanhuiyang@icloud.com>
2023-03-06 13:43:24 +0800

    增加 pagination 分頁功能

--- reactapp/src/Amy/components/Bar/index.js
+++ reactapp/src/Amy/components/Bar/index.js
@@ -1,19 +1,16 @@
-import { useMediaQuery } from '@mui/material'
-import { useTheme } from '@mui/material/styles'
-import BarDesktop from './BarDesktop'
-import BarMobile from './BarMobile'
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import BarDesktop from "./BarDesktop";
+import BarMobile from "./BarMobile";
 
 export default function Bar() {
-  const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.down('md'))
 
-  return (
-    <>
-      {matches ? (
-        <BarMobile matches={matches} />
-      ) : (
-        <BarDesktop matches={matches} />
-      )}
-    </>
-  )
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.down('md'));
+
+	return(
+		<>
+			{matches ? <BarMobile matches={matches}/>: <BarDesktop matches={matches}/>}
+		</>
+	);
 }
--- reactapp/src/Amy/components/ProductDetail/index.js
+++ reactapp/src/Amy/components/ProductDetail/index.js
@@ -1,121 +1,127 @@
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  Slide,
-  Box,
-  IconButton,
-  useMediaQuery,
-  Typography,
-  Button,
-} from '@mui/material'
-import { shades } from '../../../styles/theme'
-import CloseIcon from '@mui/icons-material/Close'
-import styled from '@emotion/styled'
-import { useTheme } from '@mui/material/styles'
-import { Product, ProductImage } from '../../../styles/Product/index'
-import Count from '../Count/index'
-import FacebookIcon from '@mui/icons-material/Facebook'
-import TwitterIcon from '@mui/icons-material/Twitter'
-import InstagramIcon from '@mui/icons-material/Instagram'
-import FavoriteIcon from '@mui/icons-material/Favorite'
+import { Dialog, DialogTitle, DialogContent, Slide, Box, IconButton, useMediaQuery, Typography, Button } from "@mui/material";
+import { shades }  from "../../styles/theme";
+import CloseIcon from "@mui/icons-material/Close";
+import styled from "@emotion/styled";
+import { useTheme } from "@mui/material/styles";
+import { Product, ProductImage } from "../../styles/Product/index";
+import  Count  from "../Count/index";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import useCart from "../../hooks/useCart";
+import PetsIcon from '@mui/icons-material/Pets';
 
-function SlideTransition(props) {
-  return <Slide direction="down" {...props} />
+
+function SlideTransition(props){
+  return <Slide direction="down" {...props}/>
 }
 
-const ProductDetailWrapper = styled(Box)(({ theme }) => ({
-  display: 'flex',
+
+const ProductDetailWrapper = styled(Box)( ({theme}) => ({
+  display: "flex",
   padding: theme.spacing(4),
-}))
+}));
 
-const ProductDetailInfoWrapper = styled(Box)(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-  maxWidth: 500,
+const ProductDetailInfoWrapper = styled(Box)( () => ({
+  display: "flex",
+  flexDirection: "column",
+  maxWidth: 600,
   lineHeight: 1.5,
-}))
+}));
 
-export default function ProductDetail({ open, onClose, product }) {
-  const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.down('md'))
 
-  return (
+
+export default function ProductDetail({open, onClose, product}){
+
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+
+  const { addToCart, addToCartText } = useCart(product);
+  return(
     <Dialog
-      TransitionComponent={SlideTransition}
+      TransitionComponent = {SlideTransition}
       variant="permanent"
       open={open}
-      fullScreen
-    >
-      <DialogTitle sx={{ background: shades.secondary[500] }}>
+      // fullScreen
+      >
+      <DialogTitle
+      sx={{background: shades.secondary[500]}}>
         <Box
           display="flex"
           alignItems="center"
-          justifyContent={'space-between'}
-        >
-          {/* 商品標題 */}　{product.name}
+          justifyContent={"space-between"}>
+          {/* 商品標題 */}
+          {product.name}
           <IconButton onClick={onClose}>
-            <CloseIcon />
+            <CloseIcon/>
           </IconButton>
         </Box>
       </DialogTitle>
-      <DialogContent sx={{ background: '#fff5ea' }}>
-        <ProductDetailWrapper
-          display={'flex'}
-          flexDirection={matches ? 'column' : 'row'}
-        >
-          <Product sx={{ mr: 4 }}>
-            <ProductImage src={product.image2} />
-          </Product>
-        </ProductDetailWrapper>
-        <ProductDetailInfoWrapper>
-          {/* <Typography variant="subtitle">編號: 123</Typography> */}
-          {/* <Typography variant="subtitle">庫存量: 5 在庫</Typography> */}
-          <Typography sx={{ lineHeight: 2 }} variant="h6">
-            {product.name2}
-          </Typography>
-          <Typography variant="body">
-            {product.description}
-            <br></br>
-            {product.description2}
-            <br></br>
-          </Typography>
-          <Typography variant="body">{product.description3}</Typography>
-          <Box
-            sx={{ mt: 4 }}
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Typography
-              sx={{ lineHeight: 2, color: shades.blue[500] }}
-              variant="h6"
+      <DialogContent sx={{background:'#fff5ea'}}>
+      <ProductDetailWrapper  display={"flex"} flexDirection={matches ? "column" : "row"}>
+        <Product sx={{ mr: 4 }}>
+          <ProductImage src={ product.image2 } />
+        </Product>
+      </ProductDetailWrapper>
+      <ProductDetailInfoWrapper>
+            {/* <Typography variant="subtitle">編號: 123</Typography> */}
+            {/* <Typography variant="subtitle">庫存量: 5 在庫</Typography> */}
+            <Typography sx={{ lineHeight: 2 }} variant="h6" >
+              {product.name2}
+            </Typography>
+            <Typography variant="body"  >
+              {product.description}<br></br>
+              {product.description2}<br></br>
+            </Typography>
+            <Typography variant="body"  >
+              {product.description3}
+            </Typography>
+            <Box
+              sx={{ mt: 4 }}
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
             >
+              <Typography sx={{ lineHeight: 2,color: shades.blue[500] }} variant="h6"  >
               ${product.price}
             </Typography>
-            <Count />
-            <Button variant="contained">加購物車</Button>
-          </Box>
-          <Box
-            display="flex"
-            alignItems="center"
-            sx={{ mt: 4, color: shades.beige_dark[800] }}
-          >
-            <FavoriteIcon sx={{ mr: 2 }} />
-            加入願望清單
-          </Box>
-          <Box
-            sx={{
-              mt: 4,
-              color: shades.beige_dark[800],
-            }}
-          >
-            <FacebookIcon />
-            <TwitterIcon sx={{ pl: 2 }} />
-            <InstagramIcon sx={{ pl: 2 }} />
-          </Box>
-        </ProductDetailInfoWrapper>
+            </Box>
+            <Box
+              sx={{ mt: 4 }}
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Count />
+              <Button variant="contained"
+              onClick={addToCart}
+              startIcon={<PetsIcon sx={{ color: shades.black[500] }} />}
+              >
+              {/* 加入購物車 */}
+              {addToCartText}
+              </Button>
+            </Box>
+            <Box
+              display="flex"
+              alignItems="center"
+              sx={{ mt: 4, color: shades.beige_dark[800] }}
+            >
+              <FavoriteIcon sx={{ mr: 2 }} />
+              加入願望清單
+            </Box>
+            <Box
+              sx={{
+                mt: 4,
+                color: shades.beige_dark[800],
+              }}
+            >
+              <FacebookIcon />
+              <TwitterIcon sx={{ pl: 2 }} /> 
+              <InstagramIcon sx={{ pl: 2 }} />
+            </Box>
+      </ProductDetailInfoWrapper>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
