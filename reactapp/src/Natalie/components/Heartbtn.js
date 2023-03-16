63a8e88e3052fc089753eb84afdd60dd4f85fd37
biaoger <bn8449@gmail.com>
2023-03-16 15:02:59 +0800

    收藏按鈕修正

--- reactapp/src/Natalie/components/Heartbtn.js
+++ reactapp/src/Natalie/components/Heartbtn.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import { useNavigate } from 'react-router-dom'
 
 function Heartbtn(props) {
+  const sid = JSON.parse(localStorage.getItem('user'))?.sid // 使用三元運算子判斷是否有找到sid
+  // const sid = JSON.parse(localStorage.getItem('user')).sid
   const navigate = useNavigate()
+  console.log('act', props.activity)
+  const [favorite, setFavorite] = useState(false)
 
-  const [favorite, setFavorite] = useState(
-    localStorage.getItem('itemId') === 'true'
-  )
+  useEffect(() => {
+    const getFavorite = () => {
+      const url = `http://localhost:3000/activity/alikes/${sid}`
+      fetch(url, {
+        method: 'get',
+      })
+        .then((r) => r.json())
+        .then((rData) => {
+          console.log('rdata', rData)
+          if (rData.find((d) => d.activity_id === props.activity)) {
+            setFavorite(true)
+          }
+        })
+    }
+    getFavorite()
+  }, [])
+  // localStorage.getItem('itemId') === 'true'
   const itemId = props
-  const sid = JSON.parse(localStorage.getItem('user'))?.sid
   const handleClick = async () => {
     if (!sid) {
       alert('請先登入')
