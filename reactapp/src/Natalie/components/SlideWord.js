2fb88aef3c1a12f1a652822d9224406c58ef3c01
biaoger <bn8449@gmail.com>
2023-03-17 17:49:31 +0800

    首頁完成

---
+++ reactapp/src/Natalie/components/SlideWord.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/home.css'
+
+function SlideWord() {
+  return (
+    <>
+      <div className="fontbox">
+        <div className="fonttext">
+          PawPaws PawPaws PawPaws PawPaws PawPaws PawPaws PawPaws PawPaws
+          PawPaws PawPaws PawPaws PawPaws PawPaws PawPaws PawPaws PawPaws
+          PawPaws PawPaws PawPaws PawPaws
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SlideWord
