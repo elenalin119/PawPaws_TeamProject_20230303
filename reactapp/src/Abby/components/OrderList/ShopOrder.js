6ce913b922d099aca12bb0bd7dac12d8d3f14618
quanhuiyang <quanhuiyang@icloud.com>
2023-03-17 14:32:49 +0800

    Date 台北時間

--- reactapp/src/Abby/components/OrderList/ShopOrder.js
+++ reactapp/src/Abby/components/OrderList/ShopOrder.js
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from 'react'
 import TableShop from '../../../Amy/components/Cart/TableShop'
 import axios from 'axios'
 
-
 function ShopOrder({ sid }) {
   // 使用 useEffect鉤子從後端取得訂單列表，並將其存放在 orderList 狀態
-  const [orderList, setOrderList] = useState([]);
+  const [orderList, setOrderList] = useState([])
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/shop/getOrder/${sid}`);
-        setOrderList(response.data.orders);
+        const response = await axios.get(
+          `http://localhost:3000/shop/getOrder/${sid}`
+        )
+        setOrderList(response.data.orders)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
-    fetchOrders();
-  }, [sid]);
-  
+    }
+    fetchOrders()
+  }, [sid])
+
   // const orderList3 = [
   //   {
   //     s_order_id: 167895101280613,
@@ -29,6 +30,11 @@ function ShopOrder({ sid }) {
   //   },
   // ]
 
+  // const taipeiTime = new Date(order.s_order_date).toLocaleString('zh-TW', {
+  //   timeZone: 'Asia/Taipei',
+  //   dateStyle: 'short',
+  // });
+
   return (
     <>
       {/* <TableShop
@@ -39,18 +45,32 @@ function ShopOrder({ sid }) {
       {/* 將 orderList 傳遞給 TableShop 元件和訂單細節 */}
       <TableShop
         orderList={orderList}
-        header={['s_order_id', 's_order_status', 's_order_date', 's_order_total', 's_order_pay', 's_order_ship']}
+        header={[
+          's_order_id',
+          's_order_status',
+          's_order_date',
+          's_order_total',
+          's_order_pay',
+          's_order_ship',
+        ]}
         sid={sid}
       ></TableShop>
-      {orderList.map(order => (
+      {orderList.map((order) => (
         <div key={order.s_order_id}>
           <h3>訂單編號：{order.s_order_id}</h3>
           <p>訂單狀態：{order.s_order_status}</p>
-          <p>訂購日期：{order.s_order_date}</p>
+          {/* <p>訂購日期：{order.s_order_date.substring(0, 10)}</p> */}
+          <p>
+            訂購日期：
+            {new Date(order.s_order_date).toLocaleString('zh-TW', {
+              timeZone: 'Asia/Taipei',
+              dateStyle: 'short',
+            })}
+          </p>
           <p>訂購金額：{order.s_order_total}</p>
           <p>付款方式：{order.s_order_pay}</p>
           <p>出貨狀態：{order.s_order_ship}</p>
-          </div>
+        </div>
       ))}
     </>
   )
@@ -63,7 +83,6 @@ export default ShopOrder
 // 訂單編號-訂單狀態-訂購日期-訂單金額-付款方式-出貨狀態-會員ID >>>訂單查詢（指定會員id）
 // SELECT * FROM `s_order` WHERE `s_order_user_id` = 1076;
 // http://localhost:3000/shop/getOrder/:id
-
 
 // 商品流水號-訂單編號-商品名-商品圖片-商品數量-商品合計 >>>訂單明細（指定訂單編號id）
 // SELECT * FROM `s_order_detail` WHERE `s_order_id`='167872153862858';
